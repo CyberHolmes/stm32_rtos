1ba32072776eb83aab29772bc444be89244ef520
Hailin Archer <hailin.d@gmail.com>
2026-06-08 19:27:12 -0600

    Initial commit

---
+++ Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/portable.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/list.h
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/portable.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/list.h:
---
+++ Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/portable.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/task.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/list.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/timers.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/task.h \
+ /home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/stack_macros.h
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/portable.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/task.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/list.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/timers.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/task.h:
+/home/hailin/Projects/Master_RTOS/workspace1/proj1/ThirdParty/FreeRTOS/include/stack_macros.h:
